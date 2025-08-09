63ac62925b62e14453333a359c8c491a16d1cd95
Delo <guiweck@hotmail.com>
2025-08-08 21:26:31 -0300

    commit que diz apenas verdades

---
+++ verdades.cs
@@ -0,0 +1,13 @@
+using System; 
+
+namespace MyConsoleApp 
+{
+    internal class verdades
+    {
+        static void Main(string[] args) {
+            while(true) {
+                Console.Writeline("Valorant é uma bosta.");
+            }
+            }
+        }
+}
